e571d6d6b90b8c26f998a91a84aa158888e8460b
PJSalter <45575016+PJSalter@users.noreply.github.com>
2022-09-03 15:56:08 +0100

    key listener and action listener for gameplay

--- src/GamePlay.java
+++ src/GamePlay.java
@@ -1,4 +1,35 @@
 
-public class GamePlay {
+import java.awt.event.*;
+import javax.swing.JPanel;
+// import javax.swing.event.AncestorListener;
+import javax.swing.Timer;
 
+// implementing this with a keylist listener and extending the gameplay class with JPanel
+public class GamePlay extends JPanel implements KeyListener, ActionListener{
+    // creating a boolean variable firstly
+        private boolean play = false;
+        private int score = 0;
+        private int totalBricks = 21;
+        private Timer timer;
+        private int delay = 8;
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            // TODO Auto-generated method stub
+            
+        }
+        @Override
+        public void keyTyped(KeyEvent e) {
+            // TODO Auto-generated method stub
+            
+        }
+        @Override
+        public void keyPressed(KeyEvent e) {
+            // TODO Auto-generated method stub
+            
+        }
+        @Override
+        public void keyReleased(KeyEvent e) {
+            // TODO Auto-generated method stub
+            
+        }
 }
